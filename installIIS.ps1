204672d3af5034c2c8a8e58339ea358a8fd4135b
lthomasGIT <63045868+lthomasGIT@users.noreply.github.com>
2020-04-02 11:51:47 +0200

    Add files via upload

---
+++ installIIS.ps1
@@ -0,0 +1,7 @@
+# Install IIS.
+Install-WindowsFeature -name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Bienvenue sur le serveur Web Azure de Loic GTEK! My name is $($env:computername).</h2></body></html>"
